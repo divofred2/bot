1fef056238af035e81d667c2df7f61f2d8cfe6f6
divofred <fredrickemmanuelc@gmail.com>
2023-04-19 12:15:31 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+client.on('message', message => {
+  const lowerMessage = message.body.toLowerCase();
+  console.log(lowerMessage);
+  if (lowerMessage.includes('btc') || lowerMessage.includes('bitcoin')) {
+    message.reply('Current bitcoin rate is $29,154.00');
+  }
+  if (lowerMessage.includes('eth') || lowerMessage.includes('ethereum')) {
+    message.reply('Current ethereum rate is $1,971.97');
+  }
+});
+client.initialize();
