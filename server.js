3824e45571fc70e6ea7e3d35e477a2c071f8c38d
divofred <fredrickemmanuelc@gmail.com>
2023-04-19 16:01:19 +0100

    first-commit

--- server.js
+++ server.js
@@ -15,21 +15,32 @@ client.on('qr', qr => {
 client.on('ready', () => {
   console.log('Client is ready!');
 });
-
+let btc;
+let eth;
 client.on('message', message => {
   const lowerMessage = message.body.toLowerCase();
   if (lowerMessage.includes('set')) {
     let setting = lowerMessage.split(' ');
-    console.log(setting);
+    let sto = setting[1];
+    let val = setting[3];
+
+    if (sto == 'eth') {
+      eth = val;
+      console.log(eth);
+    }
+    if (sto === 'btc') {
+      btc = val;
+      console.log(btc);
+    }
   } else {
     if (lowerMessage.includes('btc') || lowerMessage.includes('bitcoin')) {
-      message.reply('Current bitcoin rate is $29,154.00');
+      message.reply(`Current bitcoin rate is ${btc}`);
     }
-    if (lowerMessage.includes('Hello')) {
+    if (lowerMessage.includes('hello')) {
       message.reply('Hi');
     }
     if (lowerMessage.includes('eth') || lowerMessage.includes('ethereum')) {
-      message.reply('Current ethereum rate is $1,971.97');
+      message.reply(`Current ethereum rate is ${eth}`);
     }
   }
 });
